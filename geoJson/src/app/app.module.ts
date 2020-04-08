fedc2bca8bc543e590a7aecd89db63b59b2caf8e
Bicanic-Alen <bicanic.alen@iisgalvanimi.edu.it>
2020-04-08 17:56:48 +0000

    0.1

---
+++ geoJson/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AgmCoreModule.forRoot({apiKey:'AIzaSyCMHafCCqdagyTbAMwvN9eqZZD-w9afGuM'})
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
